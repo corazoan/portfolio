aee1d0f200712f8da40498682bc83163d2a9c17a
Anuj Gupta <anujgupta4155@gmail.com>
2026-01-17 07:23:10 +0530

    Add vercel adapter for server side render

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+
   vite: {
     resolve: {
       alias: {
@@ -15,12 +18,17 @@ export default defineConfig({
       },
     },
   },
+
   output: "static",
+
   build: {
     inlineStylesheets: "auto",
   },
+
   server: {
     host: true,
     port: 4321,
   },
+
+  adapter: vercel(),
 });
